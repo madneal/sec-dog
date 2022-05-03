285a66d1f167cad88b4f74fc9ce3434e4efc46c5
madneal <neal1991@sina.com>
2022-05-03 15:57:22 +0800

    fix for the cleanup task

--- src/main/kotlin/com/github/madneal/secdog/Checker.kt
+++ src/main/kotlin/com/github/madneal/secdog/Checker.kt
@@ -1,14 +1,6 @@
 package com.github.madneal.secdog
 
 import com.google.gson.Gson
-import com.google.gson.JsonArray
-import com.google.gson.JsonObject
-import org.codehaus.jettison.json.JSONTokener
-import java.io.BufferedReader
-import java.io.DataOutputStream
-import java.io.InputStreamReader
-import java.lang.StringBuilder
-import java.net.HttpURLConnection
 import java.net.URL
 import java.net.URLEncoder
 
